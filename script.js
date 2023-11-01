c49c1bba18fe3641423eb823e04540f43630668a
Jaachimma.Umeozor <umehandels@gmail.com>
2023-11-01 13:36:58 +0100

    preloader

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const navBar = document.getElementById('navBar');
 function togglebtn(){
   navBar.classList.toggle('hidemenu')
 }
+
+/*------Preloader------*/
+const loaderPage = document.getElementById('preloader');
+window.addEventListener("load", ()=>{
+  loaderPage.style.display="none";
+})
